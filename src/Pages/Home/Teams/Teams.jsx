b3a2d85483c2d710ecd6871a94b20b069701dbc1
Kj-RahiL <rahiilarham@gmail.com>
2023-11-25 19:33:17 +0600

    complete testimonials

--- src/Pages/Home/Teams/Teams.jsx
+++ src/Pages/Home/Teams/Teams.jsx
@@ -1,10 +1,12 @@
 
 import { useEffect, useState } from 'react';
 import 'swiper/css/pagination';
-
-// import required modules
 import { FreeMode, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
 const Teams = () => {
     const [items, setItems] = useState([])
     useEffect(() => {
@@ -12,10 +14,13 @@ const Teams = () => {
             .then(res => res.json())
             .then(data => setItems(data))
     }, [])
+   
     return (
-        <div>
-            <p className="text-2xl font-bold">Our Strongest Team</p>
-            <h2 className="text-[#F4AF00] text-4xl font-semibold">MEET EXPERT TRAINERS</h2>
+        <div className='my-20'>
+            <div className='text-center my-14'>
+                <p className="text-3xl font-bold">Our Strongest Team</p>
+                <h2 className="text-[#F4AF00] text-4xl font-semibold">MEET EXPERT TRAINERS</h2>
+            </div>
             <Swiper
                 slidesPerView={3}
                 spaceBetween={30}
@@ -32,9 +37,13 @@ const Teams = () => {
                             <figure className="h-[576px]">
                                 <img src={item.image} alt="Shoes" className="rounded-xl h-full" />
                             </figure>
-                            <div className="card-body items-center text-center absolute top-0 left-0 w-full h-full pt-4 bg-opacity-40 bg-black opacity-0 hover:opacity-100 transition-opacity duration-300"> 
+                            <div className="card-body items-center text-center absolute top-0 left-0 w-full h-full pt-14 bg-opacity-50 bg-black opacity-0 hover:opacity-100 transition-opacity duration-300">
+                                <h3>{item.name}</h3>
                                 <h2 className="card-title text-2xl font-bold">{item.title}</h2>
-                                <p className="text-base text-gray-400">{item.description}</p>
+                                <div className='flex text-4xl gap-10 mt-5'>
+                                    <Link to={item.socialLinks.facebook}><FaFacebook className='text-blue-500 bg-white rounded-full'></FaFacebook></Link>
+                                    <FaTwitter className='text-blue-500'></FaTwitter>
+                                </div>
                             </div>
                         </div>
                     </SwiperSlide>)
--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,8 +1,67 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { useEffect, useState } from 'react';
+import '@smastrom/react-rating/style.css'
+import { Rating } from '@smastrom/react-rating';
+
+import background from '/bg.svg'
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('/reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+
     return (
-        <div>
-            <h3>this is testimonials</h3>
+        <div >
+            <div className='text-center my-5 mt-10 space-y-2'> 
+                <h3 className='text-4xl uppercase font-bold text-[#F4AF00]'>testimonials</h3>
+                <p className='text-2xl font-bold'>What Client s Say</p>
+            </div>
+
+            <div className='bg-cover py-10' style={{ backgroundImage: `url(${background})`}}>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper "
+                >
+                    <div >
+                        {
+                            reviews.map(review => <SwiperSlide key={review.id}
+                            >
+
+                                <div className="mx-24 flex flex-col justify-center items-center">
+                                    <Rating
+                                        style={{ maxWidth: 180 }}
+                                        value={review.rating}
+                                        readOnly
+                                    />
+                                    <p className="py-5 w-3/4 text-center text-xl">{review.details}</p>
+                                    <div className='text-center mb-10'>
+                                        <h2 className="text-[#CD9003] text-3xl font-semibold">{review.name}</h2>
+                                        <p className='text-gray-400'>{review.title}</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>)
+                        }
+                    </div>
+
+                </Swiper>
+            </div>
         </div>
     );
 };
